8306275498f95f5e7c2a92cb1e692cc66e6bd75c
Komal Bansal <komalb1408@gmail.com>
2017-04-05 07:55:08 -0700

    Update todoctrl.js

--- to-do app/js/todoctrl.js
+++ to-do app/js/todoctrl.js
@@ -1,11 +1,34 @@
+app.controller("todoctrl",function($scope,Model){
+    $scope.taskObject={
+      taskList:[],
+      
+      addTask:function(){
+        var newtask= new Model($scope.todoTask);
+          console.log(newtask);
+//          $scope.todoTask.focus();
+          $scope.todoTask="";
+        return this.taskList.push(newtask);
+          
+    },
+    
+    removeCompletedTask:function(){
+        this.taskList= this.taskList.filter(function(x){
+            return !x.isCompleted;
+        });
+    },
+    
+    countCompleted:function(){
+        return this.taskList.filter(function(x){
+            return x.isCompleted;
+        }).length;
+    },
+        
+countPending:function(){
+        return this.taskList.filter(function(x){
+            return !x.isCompleted;
+        }).length;
+    }
+    };
+})
 
-    app.controller("todoctrl",function($scope){
-        $scope.arr=[];
-        $scope.addtask= function(){
-            $scope.arr.push($scope.task);
-        }
-        $scope.deletetask=function(x){
-            $scope.arr.splice(x,1);
-        }
-       
-    });
+
